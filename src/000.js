0ede3aa6fbca044ef731337a4b6361d6ef58f565
sskalnik <sskalnik@sskalnik.com>
2022-02-10 06:38:32 +0000

    Add specific error messages. Change callback variable name to more closely match https://nodejs.org/api/child_process.html#child_processexeccommand-options-callback

--- src/000.js
+++ src/000.js
@@ -4,8 +4,9 @@ const port = 3000;
 
 app.get('/', function(req, res) {
   const {exec} = require('child_process');
-  exec('bin/001', (err, stdout, stderr) => {
-    if (err) {
+  exec('bin/001', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Running exec against bin/001 resulted in an error: ${error}`);
       return res.send(`${stderr}`);
     }
     return res.send(`${stdout}`);
@@ -14,35 +15,55 @@ app.get('/', function(req, res) {
 
 app.get('/aws', function(req, res) {
   const {exec} = require('child_process');
-  exec('bin/002', (err, stdout, stderr) => {
+  exec('bin/002', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Running exec against bin/002 resulted in an error: ${error}`);
+      return res.send(`${stderr}`);
+    }
     return res.send(`${stdout}`);
   });
 });
 
 app.get('/docker', function(req, res) {
   const {exec} = require('child_process');
-  exec('bin/003', (err, stdout, stderr) => {
+  exec('bin/003', (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Running exec against bin/003 resulted in an error: ${error}`);
+      return res.send(`${stderr}`);
+    }
     return res.send(`${stdout}`);
   });
 });
 
 app.get('/loadbalanced', function(req, res) {
   const {exec} = require('child_process');
-  exec('bin/004 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
+  exec('bin/004 ' + JSON.stringify(req.headers), (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Running exec against bin/004 resulted in an error: ${error}`);
+      return res.send(`${stderr}`);
+    }
     return res.send(`${stdout}`);
   });
 });
 
 app.get('/tls', function(req, res) {
   const {exec} = require('child_process');
-  exec('bin/005 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
+  exec('bin/005 ' + JSON.stringify(req.headers), (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Running exec against bin/005 resulted in an error: ${error}`);
+      return res.send(`${stderr}`);
+    }
     return res.send(`${stdout}`);
   });
 });
 
 app.get('/secret_word', function(req, res) {
   const {exec} = require('child_process');
-  exec('bin/006 ' + JSON.stringify(req.headers), (err, stdout, stderr) => {
+  exec('bin/006 ' + JSON.stringify(req.headers), (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Running exec against bin/006 resulted in an error: ${error}`);
+      return res.send(`${stderr}`);
+    }
     return res.send(`${stdout}`);
   });
 });
